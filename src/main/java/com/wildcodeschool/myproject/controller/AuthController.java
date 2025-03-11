aa56de10981173e74d01b7aa5b8b0764c5d3bbe4
Marie-Sail <marie.saillard.pro@gmail.com>
2025-03-11 16:57:06 +0100

    Challenge13

--- src/main/java/com/wildcodeschool/myproject/controller/AuthController.java
+++ src/main/java/com/wildcodeschool/myproject/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.wildcodeschool.myproject.controller;
 
+import com.wildcodeschool.myproject.dto.UserLoginDTO;
 import com.wildcodeschool.myproject.dto.UserRegistrationDTO;
 import com.wildcodeschool.myproject.model.User;
+import com.wildcodeschool.myproject.security.AuthenticationService;
 import com.wildcodeschool.myproject.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +18,11 @@ import java.util.Set;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthenticationService authenticationService) {
         this.userService = userService;
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping("/register")
@@ -29,5 +33,14 @@ public class AuthController {
                 Set.of("ROLE_USER") // Par défaut, chaque utilisateur aura le rôle "USER"
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticate(@RequestBody UserLoginDTO userLoginDTO) {
+        String token = authenticationService.authenticate(
+                userLoginDTO.getEmail(),
+                userLoginDTO.getPassword()
+        );
+        return ResponseEntity.ok(token);
     }
 }
--- src/main/java/com/wildcodeschool/myproject/model/User.java
+++ src/main/java/com/wildcodeschool/myproject/model/User.java
@@ -1,12 +1,18 @@
 package com.wildcodeschool.myproject.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,8 +39,40 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return  roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 
     public void setPassword(String password) {
